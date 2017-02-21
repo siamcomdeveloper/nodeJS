754477841f27a3fafb80328f13e8cf7e7f1e594a
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 20:31:59 +0700

    node JS

--- ejs_template_engin.js
+++ ejs_template_engin.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express();
 //set view
 app.set('view engine','ejs');
-app.get('/profiles',function(req,res){
-  res.render('profiles');
+app.get('/profiles/:name',function(req,res){
+  var data={user:"siam",job:"developer",age:27};
+  res.render('profiles',{pserson:req.params.name,user:data});
 });
 app.listen(8080);
