8c7b003e4eab379d09093da346552b141732c670
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 21:12:31 +0700

    node JS

--- post_method.js
+++ post_method.js
@@ -3,10 +3,10 @@ var express = require('express');
 var app = express();
 //set view
 app.set('view engine','ejs');
-app.get('/showForm',function(req,res){
+app.post('/showForm',function(req,res){
   res.sendFile(__dirname + "/" + "form.html");
 });
-app.get('/showData',function(req,res){
+app.post('/showData',function(req,res){
   data={
     fname:req.query.fname,
     lname:req.query.lname
