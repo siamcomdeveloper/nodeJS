e8259dbe473b4c89be21f28f1fad1a0d7b3615ee
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 18:55:59 +0700

    node JS

---
+++ middleware.js
@@ -0,0 +1,11 @@
+console.log('Middleware');
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+  res.send("<h1>Helloworld</h1>");
+});
+//Midleware = use
+app.use('/user/about/:name',function(req,res,next){
+  console.log("Requrest : " + new Date() , req.method , req.url);
+});
+app.listen(8080);
