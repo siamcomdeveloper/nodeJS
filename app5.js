684ebbdfe762e4b57b89971c211d02e92dd8714d
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 15:37:43 +0700

    node JS

--- app5.js
+++ app5.js
@@ -1,6 +1,6 @@
 console.log('Create Server');
 var http = require('http');
 http.createServer( function(req,res) {
-  res.writeHead(200,{'content-Type':'text/plain'});
+  res.writeHead(200,{'Content-Type':'text/plain'});
   res.end("Helloworld");
 }).listen(8081,'54.169.196.245');
