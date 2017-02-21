00642c58d141176659c77ca11121ca16d8dd8c94
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 20:23:16 +0700

    node JS

--- ejs_template_engin.js
+++ ejs_template_engin.js
@@ -2,7 +2,7 @@ console.log('EJS Template Engin');
 var express = require('express');
 var app = express();
 //set view
-app.set('view engin','ejs');
+app.set('view engine','ejs');
 app.get('/profiles',function(req,res){
   res.render('profiles');
 });
