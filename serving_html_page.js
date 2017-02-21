fdefb6909173231ac3147712c1ad1df61f2a6864
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 18:10:15 +0700

    node JS

--- serving_html_page.js
+++ serving_html_page.js
@@ -3,6 +3,6 @@ var fs = require('fs');
 var http = require('http');
 http.createServer(function(req,res){
   res.writeHead(200,{'content-Type':"text/planin"});
-  var myStream = fs.createReadStream(__dirname+"/"+'index.html',utf8');
+  var myStream = fs.createReadStream(__dirname+"/"+'index.html','utf8'');
   myStream.pipe(res);
 }).listen(8080);
