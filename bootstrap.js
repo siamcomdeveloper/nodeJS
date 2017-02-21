b0747304b84a0bcdf564e48e6c671b02bb4e8428
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-22 01:06:59 +0700

    node JS

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+app.use('/static', express.static('public'));
+
 app.set('view engine', 'ejs');
 app.get("/bootstrap", function(req, res){
   res.render("main")
