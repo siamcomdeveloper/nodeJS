8dd1a20650eb1e0fc319db8fa846b3e540e49749
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 15:51:07 +0700

    node JS

--- app6.js
+++ app6.js
@@ -5,7 +5,7 @@ console.log(__dirname);
 function printHello(){
   console.log("Hello");
 }
-var t = setTimeout(printHello,1000);
+var t = setTimeout(printHello,100);
 clearTimeout(t);
 
 setInterval(printHello,2000);
