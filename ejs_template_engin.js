51f321c35f226f55570ff849017ca697a32ca427
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 20:55:11 +0700

    node JS

--- ejs_template_engin.js
+++ ejs_template_engin.js
@@ -3,15 +3,12 @@ var express = require('express');
 var app = express();
 //set view
 app.set('view engine','ejs');
-/*app.get('/profiles/:id',function(req,res){
-  var data={name:"siam",job:"developer",age:27};
-  res.render('profiles',{id:req.params.id,user:data});
-});*/
 app.get('/',function(req,res){
   res.render('index');
 });
-app.get('/profiles',function(req,res){
-  res.render('profiles');
+app.get('/profiles/:id',function(req,res){
+  var data={name:"siam",job:"developer",age:27};
+  res.render('profiles',{id:req.params.id,user:data});
 });
 app.get('/admin',function(req,res){
   res.render('database');
