12abbe4aa29cc692684ec8f2ea27d432d86afb81
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 18:36:10 +0700

    node JS

---
+++ routing.js
@@ -0,0 +1,22 @@
+console.log('Routing');
+var fs = require('fs');
+var http = require('http');
+http.createServer(function(req,res){
+  if(res.url === '/home' || req.url ==='/' ){
+    res.writeHead(200,{'content-Type':"text/html"});//200 = Ok
+    var myStream = fs.createReadStream(__dirname + "/" + 'index.html' ,'utf8');
+  }
+  else if(res.url === '/about'){
+    res.writeHead(200,{'content-Type':"text/html"});
+    var myStream = fs.createReadStream(__dirname + "/" + 'about.html' ,'utf8');
+  }
+  else if(res.url === '/contact'){
+    res.writeHead(200,{'content-Type':"text/html"});
+    var myStream = fs.createReadStream(__dirname + "/" + 'contact.html' ,'utf8');
+  }
+  else{
+    res.writeHead(404,{'content-Type':"text/html"});//404 = Error
+    var myStream = fs.createReadStream(__dirname + "/" + 'notfound.html' ,'utf8');
+  }
+  myStream.pipe(res);
+}).listen(8080);
