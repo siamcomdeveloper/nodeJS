5d041b02f0944c4d80c395d47de7cec02a125534
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 23:22:25 +0700

    node JS

--- database.js
+++ database.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var mysql = require('mysql');
 var app = express();
-var con = mysql.createConnection({
+var con = mysql.createPool({//createConnection({
 // propertis...
+  connectionLimit: 50,//connection per second
   host: 'localhost',
   user: 'root',
   password: '123456',
   database: 'blog'
 });
 
+app.get('/', function(req, resp){
+  con.getConnection(function(err, tempCont){
+
+    if(err){
+      tempCont.release();
+      console.log('Error ' + err.stack);
+    } else{
+        console.log('Connected');
+        tempCont.query("SELECT * FROM blogs", function(err, rows, fields){
+          if(err){
+            console.log('Error in the query ' + err.stack);
+          } else{
+            console.log('Successful query');
+            console.log(rows);
+            console.log('rows[0] = ' + rows[0]);
+            console.log('rows[0].topic = ' + rows[0].topic);
+            resp.send("Topic is " + rows[0].topic + "<br>");
+            resp.json(rows);
+          }
+        });
+     }
+
+  });
+});
+
+app.listen(8080);
+
+/*
 con.connect(function(err){
   if(err){
     console.log('Error');
@@ -30,5 +59,4 @@ app.get('/', function(req, resp){
     }
   });
 });
-
-app.listen(8080);
+*/
