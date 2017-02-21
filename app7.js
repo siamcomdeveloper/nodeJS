a87051ec2ee906991a729094c079e50905a32a0b
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 15:56:54 +0700

    node JS

--- app7.js
+++ app7.js
@@ -3,3 +3,6 @@ var buf = new Buffer(10);
 console.log(buf);
 var buf2 = new Buffer("siam",'utf8');
 console.log(buf2.toString());
+var buf3 = new Buffer(200);
+len = buf3.write("SIAM");
+console.log(len);
