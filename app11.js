411d42341a3edd8e9ecaa627b20c68d12539b33f
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 17:27:52 +0700

    node JS

--- app11.js
+++ app11.js
@@ -2,8 +2,4 @@ console.log("Pipe");
 var fs = require('fs');
 var reader = fs.createReadStream('code.txt');
 var writer = fs.createWriteStream('file1.txt');
-writer.write(data,'uft8');
-writer.on('finish',function(){
-  console.log("Output Finish");
-});
 reader.pipe(writer);
