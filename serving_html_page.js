48f037f748fbde583f64081002d131a3a3c34e80
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 17:53:25 +0700

    node JS

--- serving_html_page.js
+++ serving_html_page.js
@@ -1,6 +1,13 @@
-console.log('Create Server');
+/*console.log('Create Server');
 var http = require('http');
 http.createServer(function(req,res){
   res.writeHead(200,{'content-Type':"text/planin"});
+  res.end("Helloworld");
+}).listen(8080);*/
+
+console.log('Create Server');
+var http = require('http');
+http.createServer( function(req,res) {
+  res.writeHead(200,{'Content-Type':'text/plain'});
   res.end("Helloworld");
 }).listen(8080);
