7f9b2d7f48348de44fa2d632a72fe7450579cd7d
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 15:59:11 +0700

    node JS

--- app7.js
+++ app7.js
@@ -1,11 +1,20 @@
 console.log("Buffer");
 var buf = new Buffer(10);
 console.log(buf);
+
 var buf2 = new Buffer("siam",'utf8');
 console.log(buf2.toString());
+
 var buf3 = new Buffer(200);
 len = buf3.write("SIAM");
 console.log(len);
+
 var buf4 = new Buffer(4);
 buf4.write("SIAM");
 console.log(buf4.toString());
+
+var buf5 = new Buffer(25);
+for (var i = 0; i < 25; i++){
+  buf5[i] = i + 97;
+}
+console.log(buf5.toString());
