6716c4d979086b5b795e18e7f73d493dd6c41615
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 18:47:27 +0700

    node JS

--- express.js
+++ express.js
@@ -4,4 +4,7 @@ var routing = express();
 routing.get('/',function(req,res){
   res.send("<h1>Helloworld</h1>");
 });
+routing.get('/about',function(req,res){
+  res.send("<h1>About</h1>");
+});
 routing.listen(8080);
