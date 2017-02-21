43aedbb0d41f8d46ca2116ff14c776aac0a73a62
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-21 17:09:44 +0700

    node JS

--- app8.js
+++ app8.js
@@ -1,10 +1,5 @@
 
 console.log("Buffer JSON");
-
-var buf = new Buffer("Siam Ngan",'utf8');
-var json = buf.toJSON(buf.toString());
-console.log(json);
-
 var buf1 = new Buffer("siam");
 var buf2 = new Buffer("ngan");
 var buf3 = Buffer.concat([buf1,buf2]);
