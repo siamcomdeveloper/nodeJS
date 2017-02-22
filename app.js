ffaa1741c12fb1229a96b40aecec3c0626abe3ba
siamcomdeveloper <siamcomdeveloper@gmail.com>
2017-02-22 16:59:53 +0700

    node JS

---
+++ app.js
@@ -0,0 +1,39 @@
+/**
+ * Module dependencies.
+ */
+var http = require('http');
+var express = require('express');
+var mysql = require('mysql');
+var connection  = require('express-myconnection');
+//load blogs route
+var routes = require('./routes');
+var blogs = require('./routes/blogs');
+var app = express();
+//set and use module
+app.set('view engine', 'ejs');
+app.use('/static', express.static('public'));
+app.use(express.json());
+/*------------------------------------------
+    connection peer, register as middleware
+    type koneksi : single,pool and request
+-------------------------------------------*/
+app.use(
+    connection(mysql,{
+        host: 'localhost',
+        user: 'root',
+        password: '123456',
+        port : 3306, //port mysql
+        database: 'blog'
+    },'request')
+);
+app.get('/', routes.index);//route index
+app.get('/blogs', blogs.list);//route blog list
+app.get('/blogs/add', blogs.add);//goto form new blog
+app.post('/blogs/add', blogs.save);//insert blog
+app.get('/blogs/delete/:id', blogs.delete_blog);
+app.get('/blogs/edit/:id', blogs.edit);//goto form edit blog
+app.post('/blogs/edit/:id',blogs.save_edit);//edit blog route , get n post
+app.use(app.router);
+http.createServer(app).listen(8080, function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/blogs.js
@@ -0,0 +1,115 @@
+/*
+ * GET blogs listing.
+ */
+
+//list = select All
+exports.list = function(req, res){
+  req.getConnection(function(err,connection){
+
+     connection.query('SELECT * FROM blog',function(err,rows)     {
+
+        if(err)
+           console.log("Error Selecting : %s ",err );
+
+            res.render('showlist',{page_title:"All blogs",data:rows});
+
+         });
+
+    });
+
+};
+
+exports.add = function(req, res){
+  res.render('showform',{page_title:"Add blogs",url:'/blogs/add'});
+};
+
+exports.edit = function(req, res){
+
+  var id = req.params.id;
+
+  req.getConnection(function(err,connection){
+
+     connection.query('SELECT * FROM blogs WHERE id = ?',[id],function(err,rows)
+        {
+
+            if(err)
+                console.log("Error Selecting : %s ",err );
+
+            res.render('showform',{page_title:"Edit blogs",url:'/blogs/edit',data:rows});
+
+         });
+
+    });
+};
+
+/*Add blog = Create & Insert the blog*/
+exports.save = function(req,res){
+
+    var input = JSON.parse(JSON.stringify(req.body));
+
+    req.getConnection(function (err, connection) {
+
+        var data = {
+          topic    : input.topic,
+          content : input.content
+        };
+
+        var query = connection.query("INSERT INTO blogs set ? ",data, function(err, rows)
+        {
+
+          if (err)
+              console.log("Error inserting : %s ",err );
+
+          res.redirect('/blogs');
+
+        });
+
+       // console.log(query.sql); get raw query
+
+    });
+};
+
+/*Edit = Edit & Update edited blog*/
+exports.save_edit = function(req,res){
+
+    var input = JSON.parse(JSON.stringify(req.body));
+    var id = req.params.id;
+
+    req.getConnection(function (err, connection) {
+
+        var data = {
+            topic    : input.topic,
+            content : input.content
+        };
+
+        connection.query("UPDATE blogs set ? WHERE id = ? ",[data,id], function(err, rows)
+        {
+
+          if (err)
+              console.log("Error Updating : %s ",err );
+
+          res.redirect('/blogs');
+
+        });
+
+    });
+};
+
+exports.delete_blog = function(req,res){
+
+     var id = req.params.id;
+
+     req.getConnection(function (err, connection) {
+
+        connection.query("DELETE FROM blogs  WHERE id = ? ",[id], function(err, rows)
+        {
+
+             if(err)
+                 console.log("Error deleting : %s ",err );
+
+             res.redirect('/blogs');
+
+        });
+
+     });
+};
